49664899aea425b169d05b1d6da979cd3abdb9f4
Cyan <cyankindasus@protonmail.com>
2022-05-07 20:23:01 +0800

    change substr(deprecated) to slice, warn user to enable popups....

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-let params = new URLSearchParams(location.hash.substr(1));
+let params = new URLSearchParams(location.hash.slice(1));
+
 let token = params.get("token");
 if (token) {
-	new QRCode(document.getElementById("qrcode"), {
-		text: token,
-		width: 180,
-		height: 180,
-	});
+	new QRCode(document.getElementById("qrcode"), { text: token, width: 180, height: 180 });
 	let el = document.createElement("pre");
 	el.innerHTML = token;
 	document.body.appendChild(el);
