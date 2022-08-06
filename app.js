a8c5956588bec84894ce1802e3fb1dc6ec3adb84
cyan-2048 <72785466+cyan-2048@users.noreply.github.com>
2022-08-06 12:33:50 +0800

    fix

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let params = new URLSearchParams(location.hash.slice(1));
+const params = new URLSearchParams(location.hash.slice(1));
 
-let token = params.get("token");
+const token = params.get("token");
 if (token) {
 	new QRCode(document.getElementById("qrcode"), { text: token, width: 180, height: 180 });
-	let el = document.createElement("pre");
+	const el = document.createElement("pre");
 	el.innerHTML = token;
 	document.body.appendChild(el);
 }
