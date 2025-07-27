62b9aa2edd709beec8c864daf4abb5cf293d03b8
cyan2048 <72785466+cyan-2048@users.noreply.github.com>
2025-07-28 01:49:06 +0800

    qol changes

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const params = new URLSearchParams(location.hash.slice(1));
+(() => {
+  const qrcode = document.getElementById("qrcode");
+  const root = document.getElementById("root");
 
-const token = params.get("token");
-if (token) {
-	new QRCode(document.getElementById("qrcode"), { text: token, width: 180, height: 180 });
-	const el = document.createElement("pre");
-	el.innerHTML = token;
-	document.body.appendChild(el);
-}
+  function render() {
+    root.innerHTML = qrcode.innerHTML = "";
+
+    const params = new URLSearchParams(location.hash.slice(1));
+
+    const token = params.get("token");
+    if (token) {
+      const el = document.createElement("pre");
+      el.innerText = token;
+      root.appendChild(el);
+
+      new QRCode(qrcode, { text: token, width: 180, height: 180 });
+    }
+  }
+
+  render();
+
+  window.onhashchange = render;
+})();
